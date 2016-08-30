8d5906afd7fe3fd561e86a20cef66ed52507eb3b
Yale Work <student@appacademy.io>
2016-08-29 22:43:29 -0700

    working on frontend auth, got to session reducer, where there's a debugger. Going forwards through cycle.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def log_in!(user)
+    session[:session_token] = user.session_token
+  end
+
+  def current_user
+    User.find_by_session_token(:session_token)
+  end
+
+  def log_out!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
 end
